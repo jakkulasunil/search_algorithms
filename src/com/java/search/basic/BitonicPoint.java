e4b93e350ccb39a4ad52a6d356b1f8be8c0fa633
suniljakkula <sunij8911@gmail.com>
2026-01-07 07:56:18 +0530

    Bitonic Point

---
+++ src/com/java/search/basic/BitonicPoint.java
@@ -0,0 +1,23 @@
+package com.java.search.basic;
+
+public class BitonicPoint {
+
+	static int findMaximum(int[] arr) {
+		int max = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max)
+				max = arr[i];
+			else
+				break;
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 2, 4, 5, 7, 8, 3}; 
+        System.out.println(findMaximum(arr));
+	}
+
+}
